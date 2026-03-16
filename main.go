1742233dc3e51519bf63c011fba7df819b7da952
JoshCodeAligator <joshdebele8@gmail.com>
2026-03-16 14:54:38 -0600

    Server Handlers in Main for routing to appropriate microservice

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	server := http.NewServeMux()
 
+	server.HandleFunc("/users", usersHandler)
+	server.HandleFunc("/products", productsHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", server))
 }
